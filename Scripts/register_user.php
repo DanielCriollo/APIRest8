b857c947e7917b2bb4e9c3048be3c208e8a4e636
DanielCriollo <danielcriollo9706@gmail.com>
2019-10-30 20:31:07 -0500

    clase jueves

--- Scripts/register_user.php
+++ Scripts/register_user.php
@@ -1,7 +1,24 @@
 <?php
     //call connectionDB file
-    include ("Scripts/connectionDB.php");
+    include ("connectionDB.php");
 
     //Get XML input values
+
+    $Email=$_POST['email'];
+
+    //Create SQL INSERT
+
+    $sql="INSERT INTO users (email) VALUES ('$Email')";
+    
+    //Execute SQL and validate operation
+
+    $conn->query($sql);
+
+    if( $conn->query($sql)===TRUE){
+        echo "User has been registered into DataBase";
+    }else{
+        die("The error is: ".$conn->error);
+    }
+
 
     ?>
